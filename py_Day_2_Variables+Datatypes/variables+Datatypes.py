f1cfc07fb6dc37d75df6eeb8be3bfaa5a1751b1b
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-06 07:56:51 +0500

    new python variables file with code

---
+++ py_Day_2_Variables+Datatypes/variables+Datatypes.py
@@ -0,0 +1,33 @@
+# 1. Integer (int) - Whole numbers
+age = 25
+
+# 2. Float - Decimal numbers
+price = 99.99
+
+# 3. String (str) - Sequence of characters (text)
+name = "Ejaz"
+
+# 4. Boolean (bool) - True or False
+is_student = True
+
+# 5. List - Ordered, changeable collection of items
+#     => we will discuss more about lists in the coming chapter     
+colors = ["red", "green", "blue"]
+
+# 6. Dictionary (dict) - Key-value pairs
+#     => we will discuss more about dictionaries in the coming chapter
+person = {
+    "name": "Ejaz",
+    "age": 25,
+    "is_student": True
+}
+
+
+print("Age:", age) # Output: Age: 25
+print("Price:", price) # Output: Price: 99.99
+print("Name:", name)# Output: Name: Ejaz
+print("Is Student:", is_student) # Output: Is Student: True
+print("Colors:", colors)     # Output: Colors: ['red', 'green', 'blue']
+print("Person:", person) # Output: Person: {'name': 'Ejaz', 'age': 25, 'is_student': True}
+print("Person's Name:", person["name"]) # Output: Person's Name: Ejaz
+print("Person's Age:", person["age"]) # Output: Person's Age: 25
