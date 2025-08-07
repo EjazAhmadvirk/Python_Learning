8c252a80dba59cc4905af550ee2d42d42bbfbcf3
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-07 09:28:43 +0500

    add comments in the file

--- py_Day_3_input+Loops/for_Loop.py
+++ py_Day_3_input+Loops/for_Loop.py
@@ -1,2 +1,8 @@
 for i in range(5):
     print("I love Pakistan")
+
+
+
+# This code will print numbers from 0 to 4, each on a new line
+# we use the range function to generate a sequence of numbers
+# The for loop iterates over each number in that sequence and prints it
--- py_Day_3_input+Loops/input.py
+++ py_Day_3_input+Loops/input.py
@@ -1,4 +1,18 @@
-name = input("Enter your name: ")
+name = input("Enter your name: ") 
 age = int(input("Enter your age: "))
 
 print(f"Hello, {name}! You are {age} years old.")
+
+
+"""
+for simple input use input() function
+for numeric input, convert the input to an integer using int() function
+for floating point numbers, use float() function
+means if you want to take input as a number, you can use int() or float() to convert the string input to a number
+
+
+in print f is used to format the string, allowing you to include variables directly in the string using curly braces
+for example, f"Hello, {name}!" will replace {name} with the value of the variable name
+This is useful for creating dynamic strings that include variable values.
+"""
+
