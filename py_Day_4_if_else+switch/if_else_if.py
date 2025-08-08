91040bd44ffc2c2afb1419fcf03486ee8b7592bc
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-08 08:53:27 +0500

    if else file

---
+++ py_Day_4_if_else+switch/if_else_if.py
@@ -0,0 +1,9 @@
+grade = input("Enter your grade:   ")
+if grade == 'A':
+    print("Excellent!")
+elif grade == 'B':
+    print("Good job!")
+elif grade == 'C':
+    print("You passed.")
+elif grade == 'D':
+    print("You need to improve.")
