0834e7326df4fa2946277a261b1d74027061bfdc
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 10:39:01 +0500

    new files fo string

---
+++ py_Day_5_String/string_slicing.py
@@ -0,0 +1,8 @@
+text = "Python Programming"
+print(text[0:6])   # Python
+print(text[:6])    # Python
+print(text[7:])    # Programming
+print(text[-11:])  # Programming
+print(text[::2])   # Pto rgamn (step slicing)
+print(text[::-1])  # gnimmargorP nohtyP (reversed string)
+print(text[0:6:2]) # Pto (slicing with step)
