2f9f3aef42dadf00893a8db7a06e52b6e6eb3cf8
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 10:40:47 +0500

    string methods

---
+++ py_Day_5_String/String_Methods.py
@@ -0,0 +1,12 @@
+msg = "  Hello World  "
+
+print(msg.upper())       # "  HELLO WORLD  "
+print(msg.lower())       # "  hello world  "
+print(msg.strip())       # "Hello World"
+print(msg.replace("World", "Python"))  # "  Hello Python  "
+print(msg.split())       # ['Hello', 'World']
+print(msg.find("World")) # 8 (index where found)
+print(msg.count("l"))    # 3
+print(msg.startswith("  Hello"))  # True
+print(msg.endswith("World  "))  # True
+
