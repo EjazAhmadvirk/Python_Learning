75fc1a243d0fdbbedfaddf52c15fef0ce1ea1d7a
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 10:42:48 +0500

    new files

---
+++ py_Day_5_String/string_repetition.py
@@ -0,0 +1,6 @@
+a = "Hello"
+b = "Python"
+print(a + " " + b)   # Hello Python
+print(a * 3)         # HelloHelloHello
+print(b * 2)         # PythonPython
+print(a + " " + b * 2)  # Hello PythonPython
