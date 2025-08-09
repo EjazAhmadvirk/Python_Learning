dc32e7f80d707a4de2d4773ddbb0d54f35dcff50
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 10:47:42 +0500

    strings fiels here

---
+++ py_Day_5_String/String_formationg.py
@@ -0,0 +1,17 @@
+name = "Ejaz"
+age = 21
+
+
+# String formatting using f-strings python 3.6+
+print(f"My name is {name} and I am {age} years old.")
+
+
+# fstrimming with expressions
+name = "Ejaz"
+age = 21
+print(f"My name is {name} and I am {age} years old.")
+# String formatting using format() method
+print("My name is {} and I am {} years old.".format(name, age))
+# String formatting using % operator
+print("My name is %s and I am %d years old." % (name, age))
+
