d9c55aa3b632415eda87194ddab0c0792c0cdb3f
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-09 10:36:45 +0500

    new files

---
+++ py_Day_5_String/types_of_string.py
@@ -0,0 +1,18 @@
+# Single quotes
+str1 = 'Hello'
+
+# Double quotes
+str2 = "World"
+
+# Triple quotes (multi-line)
+str3 = """Hello
+World"""
+
+# Empty string
+str4 = ''
+
+
+print(str1) # output: Hello
+print(str2) # output: World
+print(str3) # output: HelloWorld
+print(str4) # output: (empty string)
