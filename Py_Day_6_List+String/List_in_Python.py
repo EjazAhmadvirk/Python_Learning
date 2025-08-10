858f3c3a4515a6cc8a7873079c17d4e52cc70b0b
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-10 09:55:22 +0500

    first files

---
+++ Py_Day_6_List+String/List_in_Python.py
@@ -0,0 +1,13 @@
+# Creating a list
+my_list = [1, 2, 3, "Hello", 4.5]
+
+# Accessing elements
+print(my_list[0])  # Output: 1
+
+# Adding an element
+my_list.append(99)
+
+# Removing an element
+my_list.remove("Hello")
+
+print(my_list)  # [1, 2, 3, 4.5, 99]
