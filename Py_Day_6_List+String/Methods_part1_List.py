75b21e7e89ed5b06debef9b228554bfa5b6d7b8c
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-10 09:59:16 +0500

    list first methods file

---
+++ Py_Day_6_List+String/Methods_part1_List.py
@@ -0,0 +1,15 @@
+# Creating a list
+my_list = [3, 1, 2, 3]
+
+# append()
+my_list.append(4)  # [3, 1, 2, 3, 4]
+
+# extend()
+my_list.extend([5, 6])  # [3, 1, 2, 3, 4, 5, 6]
+
+# insert()
+my_list.insert(2, 'Hi')  # [3, 1, 'Hi', 2, 3, 4, 5, 6]
+
+# remove()
+my_list.remove('Hi')  # [3, 1, 2, 3, 4, 5, 6]
+
