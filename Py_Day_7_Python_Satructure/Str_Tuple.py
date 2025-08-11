6137fa592111bf4a7ec0324053c63a54cd9177f9
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-11 21:44:44 +0500

    str tuple file

---
+++ Py_Day_7_Python_Satructure/Str_Tuple.py
@@ -0,0 +1,12 @@
+my_tuple = (1, 2, 3)
+# my_tuple[0] = 10  ❌  (Not allowed, immutable)
+print(my_tuple)      # (1, 2, 3)
+print(my_tuple[0])   # 1
+
+# my_tuple.append(4)  ❌  (Not allowed, immutable)
+# my_tuple[0] = 10    ❌  (Not allowed, immutable)
+
+# my_tuple.remove(2)  ❌  (Not allowed, immutable)
+# my_tuple.pop()      ❌  (Not allowed, immutable)
+
+# my_tuple.clear()    ❌  (Not allowed, immutable)  
