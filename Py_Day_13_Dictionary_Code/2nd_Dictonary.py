836130f786da0049ef7527b3e9721aef13b22acc
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-30 09:05:40 +0500

    2nd Dictonary files

---
+++ Py_Day_13_Dictionary_Code/2nd_Dictonary.py
@@ -0,0 +1,23 @@
+# Using curly braces
+dict1 = {"a": 1, "b": 2}
+
+# Using dict() function
+dict2 = dict(x=10, y=20)
+
+# Empty dictionary
+dict3 = {}
+
+# Nested dictionary
+dict4 = {
+    "student1": {"name": "Ali", "age": 20},
+    "student2": {"name": "Ejaz", "age": 21}
+}
+
+print(dict1)
+print(dict2)
+print(dict3)
+print(dict4)
+
+
+
+
