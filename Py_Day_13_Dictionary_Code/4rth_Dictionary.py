a39c8c4f01570e600ca41e46a5a0f4fb1157f362
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-30 09:09:50 +0500

    new 4rth code of dictionary add successfully

---
+++ Py_Day_13_Dictionary_Code/4rth_Dictionary.py
@@ -0,0 +1,11 @@
+#Reamoving items from the dictionary
+student = {"name": "Ejaz", "age": 21, "city": "Islamabad"}
+
+# Remove specific key
+student.pop("city")
+
+# Remove last inserted item
+student.popitem()
+
+# Remove all items
+student.clear()
