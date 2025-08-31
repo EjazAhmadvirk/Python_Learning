41772988a49d4bc48cd97bc7276b0378e07e0bbe
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-31 07:24:05 +0500

    add new Day_14_folder

---
+++ Py_Day_13_Dictionary_Code/3rd_Dictionart.py
@@ -0,0 +1,19 @@
+student = {"name": "Ejaz", "age": 21}
+
+# Add new key-value
+student["university"] = "Riphah"
+
+# Update existing value
+student["age"] = 22
+
+print(student)
+''' 
+this is the way to add new key-value in the dictionary
+'''
+
+
+
+
+
+
+
