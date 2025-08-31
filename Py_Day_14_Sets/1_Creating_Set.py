0b33a70542736e4f6fa779ab1fa24c5bfa50ee99
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-08-31 07:26:49 +0500

    first set file here

---
+++ Py_Day_14_Sets/1_Creating_Set.py
@@ -0,0 +1,10 @@
+# Creating a set
+my_set = {1, 2, 3, 4}
+print(my_set)   # {1, 2, 3, 4}
+
+# Using set() constructor
+new_set = set([1, 2, 2, 3, 4])
+print(new_set)  # {1, 2, 3, 4}  # Duplicates removed
+
+# Empty set
+empty_set = set()   # NOT {} because {} creates a dictionary
