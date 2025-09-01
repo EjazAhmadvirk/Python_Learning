390114e6efed18ce19b28b8ffcbdea37738a50d8
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-09-01 09:25:57 +0500

    tuples file successfully

---
+++ Py_Day_15_Tuples/2_Tuples_Operations.py
@@ -0,0 +1,19 @@
+# Indexing
+numbers = (1, 2, 3, 4, 5)
+print(numbers[0])   # 1
+print(numbers[-1])  # 5
+
+# Slicing
+print(numbers[1:4])  # (2, 3, 4)
+
+# Concatenation
+a = (1, 2)
+b = (3, 4)
+print(a + b)  # (1, 2, 3, 4)
+
+# Repetition
+print(a * 3)  # (1, 2, 1, 2, 1, 2)
+
+# Membership
+print(3 in numbers)  # True
+print(6 in numbers)  # False
