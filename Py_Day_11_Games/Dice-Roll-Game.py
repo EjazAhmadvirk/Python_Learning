883ed245a7a8aa31a9e8574fa1f168c9221d1247
https://github.com/EjazAhmadvirk <ejazahmadvirk091@gmail.com>
2025-09-20 00:16:42 +0500

    simple dice roll game code

---
+++ Py_Day_11_Games/Dice-Roll-Game.py
@@ -0,0 +1,14 @@
+import random
+
+player_roll = random.randint(1, 6)
+computer_roll = random.randint(1, 6)
+
+print("You rolled:", player_roll)
+print("Computer rolled:", computer_roll)
+
+if player_roll > computer_roll:
+    print("🎉 You win!")
+elif player_roll < computer_roll:
+    print("💻 Computer wins!")
+else:
+    print("It's a tie!")
