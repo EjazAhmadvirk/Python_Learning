0875bbe9ddcbafa2b51199caa3c04f26cb16a856
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2026-04-30 23:52:29 +0500

    sd

--- py_Day_4_if_else+switch/nested_if_else.py
+++ py_Day_4_if_else+switch/nested_if_else.py
@@ -1,4 +1,5 @@
 amount = int(input("Enter the amount: "))
+# if amount 5000 then select this 
 if amount < 5000:
     if amount < 5000 and amount > 3000:
         print("you can go to the mall")
@@ -6,6 +7,7 @@ if amount < 5000:
         print("you can go to the park")
     else:
         print("stay at home")
+# if amount is between 5000 to 10000 then select these 
 elif amount >= 5000 and amount < 10000:
     print("you can go to the Trip")
 else:
